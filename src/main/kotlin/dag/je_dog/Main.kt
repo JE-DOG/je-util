e7132349541f6ce73a4d11158caf07312ee9e9a9
JE_DOG <gamzat.magomedov809@gmail.com>
2026-04-28 23:15:11 +0300

    Remove echo command

--- src/main/kotlin/dag/je_dog/Main.kt
+++ src/main/kotlin/dag/je_dog/Main.kt
@@ -2,7 +2,6 @@ package dag.je_dog
 
 import dag.je_dog.presentation.notion_daily_report.NotionDailyReportSubcommand
 import dag.je_dog.presentation.pull_kaiten_tasks_to_notion.PullKaitenTasksToNotionSubcommand
-import dag.je_dog.subcommands.EchoSubcommand
 import kotlinx.cli.ArgParser
 import kotlinx.cli.ExperimentalCli
 
@@ -10,7 +9,6 @@ import kotlinx.cli.ExperimentalCli
 fun main(args: Array<String>) {
     val parser = ArgParser(programName = "je-util")
     parser.subcommands(
-        EchoSubcommand(),
         NotionDailyReportSubcommand(),
         PullKaitenTasksToNotionSubcommand(),
     )
